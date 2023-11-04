bde19f97a1c88a91b05d78c80757739ced2ec3e0
Utipn <134465056+Utipn@users.noreply.github.com>
2023-11-03 18:47:35 -0600

    Update main.tf

--- module-call/main.tf
+++ module-call/main.tf
@@ -1,4 +1,4 @@
 module "s3" {
-    ssource = "git::https://github.com/Utipn/s3-module-repo.git//s3-module?ref=v"    
+    source = "git::https://github.com/Utipn/s3-module-repo.git//s3-module?ref=v"    
   
 }
